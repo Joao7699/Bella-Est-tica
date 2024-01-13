b609c686c08c8058053b079e077a7eb634e99321
Técnico Em Informática - 2022.6 <169127512022.6@senacrjedu.local>
2024-01-12 21:17:51 -0300

    first commit

---
+++ web/footer.php
@@ -0,0 +1,126 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<section class="review" id="review">
+     <link rel="stylesheet" href="../stylesite/footerr.css">
+</head>
+<body>
+<!-- blogs section starts  -->
+
+
+<section class="review" id="review">
+
+<h1 class="sobre"> Avaliações dos <span>pacientes</span> </h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <img src="../images/quote-img.png" alt="" class="quote">
+        <p>Me sentindo realizadissima, estou me sentindo até mais confiante, autoestima lá em cima.</p>
+        <img src="../img/white1.png" class="user" alt="">
+        <h3>john deo</h3>
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+    </div>
+
+    <div class="box">
+        <img src="../images/quote-img.png" alt="" class="quote">
+        <p>Estou muito feliz com o resultado, totalmente agradecida.</p>
+        <img src="../img/latincerto.png" class="user" alt="">
+        <h3>john deo</h3>
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+    </div>
+    
+    <div class="box">
+        <img src="../images/quote-img.png" alt="" class="quote">
+        <p>Serviços impecaveis amei!</p>
+        <img src="../img/black22.png" class="user" alt="">
+        <h3>john deo</h3>
+        <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+        </div>
+    </div>
+
+</div>
+
+</section>
+
+<!-- blogs section ends -->
+
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+<h1 class="sobre"> </h1>
+
+<div class="row">
+
+    
+        <form action="">
+            <h3>Entre em contato</h3>
+            <div class="inputBox">
+                <span class="fas fa-user"></span>
+                <input type="text" placeholder="name">
+            </div>
+            <div class="inputBox">
+                <span class="fas fa-envelope"></span>
+                <input type="email" placeholder="email">
+            </div>
+            <div class="inputBox">
+                <span class="fas fa-phone"></span>
+                <input type="number" placeholder="number">
+            </div>
+            <input type="submit" value="Entre em contato" class="btn">
+        </form>
+</div>
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer" id="share">
+
+<div class="share">
+    <a href="#" class="fab fa-facebook-f"></a>
+    <a href="#" class="fab fa-instagram"></a>
+   
+
+</div>
+
+<div class="links">
+    <a href="#">Início</a>
+    <a href="#">Sobre</a>
+    <a href="#">Tratamentos</a>
+    <a href="#">especialidades</a>
+    <a href="agends.php">Agendar</a>
+</div>
+
+<div class="credit">Criado por <span>Lorrana, Marcos, Kaique e João</span> </div>
+
+</section>
+
+<!-- footer section ends -->
+, initial-scale=1.0">
+    <title>Document</title>
+</body>
+</html>
